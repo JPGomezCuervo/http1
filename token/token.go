3e43cf25399b729c4998d8b96b1e28ca1a289a0b
Juan Pablo <122913492+JPGomezCuervo@users.noreply.github.com>
2024-11-26 10:46:51 -0500

    add print methods for debuggin

--- token/token.go
+++ token/token.go
@@ -1,4 +1,5 @@
 package token
+import "fmt"
 
 type TokenType int
 
@@ -19,3 +20,27 @@ const (
         TpVersion
         TpUri
 )
+
+var tokenTypeNames = map[TokenType]string{
+        TpIllegal:  "TpIllegal",
+        TpEOF:      "TpEOF",
+        TpBody:     "TpBody",
+        TpComment:  "TpComment",
+        TpHeader:   "TpHeader",
+        TpMethod:   "TpMethod",
+        TpProtocol: "TpProtocol",
+        TpSP:       "TpSP",
+        TpVersion:  "TpVersion",
+        TpUri:      "TpUri",
+}
+
+func (t TokenType) String() string {
+        if name, ok := tokenTypeNames[t]; ok {
+                return name
+        }
+        return fmt.Sprintf("UnknownTokenType(%d)", t)
+}
+
+func (t Token) String() string {
+	return fmt.Sprintf("&{%s %q}", t.Typ, t.Lit)
+}
